0165f41caafbaf3457e297575a242449cad87cd9
Mahmoud Alaa <126196219+mahmoud-alaa77@users.noreply.github.com>
2024-10-12 21:29:13 +0300

    create routes and routing app to handle navigation

---
+++ lib/core/routing/app_routing.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:islamic_app/core/routing/routes.dart';
+import 'package:islamic_app/features/home/ui/home_screen.dart';
+import 'package:islamic_app/features/splash/splash_screen.dart';
+
+class AppRouting {
+  Route? genrateRoute(RouteSettings settings) {
+    switch (settings.arguments) {
+      case Routes.splashScreen:
+        return MaterialPageRoute(builder: (context) => const SplashScreen());
+
+      case Routes.homeScreen:
+        return MaterialPageRoute(builder: (context) => const HomeScreen());
+      default:
+        return null;
+    }
+  }
+}
