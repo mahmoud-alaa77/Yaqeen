9053142fe5233fb1f93485d93ed540348e1465dc
Mahmoud Alaa <126196219+mahmoud-alaa77@users.noreply.github.com>
2024-10-12 19:53:37 +0300

    create app color class and add colors that we need

---
+++ lib/core/theming/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColor = Color(0xFF561C24);
+  static const Color beigeColor = Color(0xFFC0C0C0);
+  static const Color customBlackColor = Color(0xFF181818);
+  static const Color customWhiteColor = Color(0xFFF4F4F4);
+}
