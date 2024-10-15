84c364fb50fec961e39a10de8bf10acd4dd12d41
Mahmoud Alaa <126196219+mahmoud-alaa77@users.noreply.github.com>
2024-10-15 17:53:37 +0300

    update app theme class

--- lib/core/theming/app_theme.dart
+++ lib/core/theming/app_theme.dart
@@ -3,17 +3,27 @@ import 'package:islamic_app/core/theming/app_colors.dart';
 
 class AppTheme {
   static final ThemeData lightMode = ThemeData(
-      primaryColor: AppColors.primaryColor,
-      scaffoldBackgroundColor: AppColors.customWhiteColor,
-
-      
-      brightness: Brightness.light
-      
-      );
+    primaryColor: AppColors.primaryColor,
+    scaffoldBackgroundColor: AppColors.customWhiteColor,
+    fontFamily: "Jomhuria",
+    brightness: Brightness.light,
+    // textTheme: TextTheme(
+    //   bodyLarge: AppTextStyles.fontBodyLarge,
+    //   bodySmall: AppTextStyles.fontBodySmall,
+    //   bodyMedium: AppTextStyles.fontBodyMeduim,
+    // ),
+    
+  );
 
   static final ThemeData darkMode = ThemeData(
-      primaryColor: AppColors.primaryColor,
-            scaffoldBackgroundColor: AppColors.customBlackColor,
-
-      brightness: Brightness.dark);
+    primaryColor: AppColors.primaryColor,
+    scaffoldBackgroundColor: AppColors.customBlackColor.withOpacity(.7),
+    fontFamily: "Jomhuria",
+    brightness: Brightness.dark,
+    // textTheme: TextTheme(
+    //   bodyLarge: AppTextStyles.fontBodyLarge,
+    //   bodySmall: AppTextStyles.fontBodySmall,
+    //   bodyMedium: AppTextStyles.fontBodyMeduim,
+    // ),
+  );
 }
