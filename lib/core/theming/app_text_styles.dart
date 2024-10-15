fd10051fce4adf33f768e43696d54c1cf8c5240f
Mahmoud Alaa <126196219+mahmoud-alaa77@users.noreply.github.com>
2024-10-15 17:53:24 +0300

    create app text styles class and add new text styles

---
+++ lib/core/theming/app_text_styles.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:islamic_app/core/theming/app_colors.dart';
+
+class AppTextStyles {
+  
+  static TextStyle font20Bold = TextStyle(
+    fontSize: 20.sp,
+    fontWeight: FontWeight.bold,
+  );
+
+  static TextStyle font23BeigeBold = TextStyle(
+      fontSize: 23.sp,
+      fontWeight: FontWeight.bold,
+      color: AppColors.beigeColor);
+  static TextStyle font22WhiteW500 = TextStyle(
+      fontSize: 22.sp,
+      fontWeight: FontWeight.w500,
+      color: AppColors.customWhiteColor);
+        static TextStyle font25WhiteW500 = TextStyle(
+      fontSize: 25.sp,
+      fontWeight: FontWeight.w500,
+      color: AppColors.customWhiteColor);
+  static TextStyle font24RedW500 = TextStyle(
+      fontSize: 24.sp,
+      fontWeight: FontWeight.w500,
+      color: AppColors.primaryColor);
+
+  static TextStyle font20W500 = TextStyle(
+    fontSize: 20.sp,
+    fontWeight: FontWeight.w500,
+  );
+
+  static TextStyle font18Bold = TextStyle(
+    fontSize: 18.sp,
+    fontWeight: FontWeight.w700,
+  );
+  static TextStyle font18W900 = TextStyle(
+    fontSize: 18.sp,
+    fontWeight: FontWeight.w900,
+  );
+  static TextStyle font16W700 = TextStyle(
+    fontSize: 16.sp,
+    fontWeight: FontWeight.w700,
+  );
+}
