e4f1a8828810340f6f10c2f30955ce2f066382bd
Mahmoud Alaa <126196219+mahmoud-alaa77@users.noreply.github.com>
2024-10-23 19:05:55 +0300

    add sign up screen to routes

--- lib/core/routing/app_routing.dart
+++ lib/core/routing/app_routing.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:islamic_app/core/routing/routes.dart';
 import 'package:islamic_app/features/home/ui/main_screen.dart';
+import 'package:islamic_app/features/sign_up.dart/ui/sign_up_screen.dart';
 import 'package:islamic_app/features/splash/splash_screen.dart';
 
 class AppRouting {
@@ -8,6 +9,9 @@ class AppRouting {
     switch (settings.name) {
       case Routes.splashScreen:
         return MaterialPageRoute(builder: (context) => const SplashScreen());
+
+      case Routes.sigiUpScreen:
+        return MaterialPageRoute(builder: (context) => const SignUpScreen());
 
       case Routes.mainScreen:
         return MaterialPageRoute(builder: (context) => const MainScreen());
--- lib/core/routing/routes.dart
+++ lib/core/routing/routes.dart
@@ -1,4 +1,5 @@
 class Routes {
   static const String splashScreen = "/splash_screen";
   static const String mainScreen = "/main_screen";
+  static const String sigiUpScreen = "/sigin_up_screen";
 }
